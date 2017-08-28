d94799aac0f65209c100872a28c315d879ef8178
Avik Nandy <avik@harbinger-industries.net>
2017-08-27 23:23:57 -0400

    1st Commit
    Laravel Dev environment setup w/ necessary composer dependencies and
    basic user / authentication / migration schemas in place. Basic link
    database seeded and link submission form app created and added to test
    environment.

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $links = \App\Link::all();
+    return view('welcome', ['links' => $links]);
+});
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+use Illuminate\Http\Request;
+
+Route::post('/submit', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'url' => 'required|max:255',
+        'description' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return back()
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $link = new \App\Link;
+    $link->title = $request->title;
+    $link->url = $request->url;
+    $link->description = $request->description;
+    $link->save();
+    return redirect('/submit');
+});
